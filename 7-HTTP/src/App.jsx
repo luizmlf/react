3dbb9f26fd9f0c724c9e9165683c0ccfa7cd0be0
Luiz Felipe Vieira <luiz.vieira@mlf.net.br>
2023-11-17 11:41:45 -0300

    update

--- 7-HTTP/src/App.jsx
+++ 7-HTTP/src/App.jsx
@@ -1,30 +1,106 @@
 import './App.css';
 
 import {useState, useEffect} from 'react';
+import {useFetch} from './hooks/useFetch'
+
+
+const url = "http://localhost:3000/products";
 
 
 function App() {
 
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-  const url = "http://localhost:3000/products"
+  
 
   // 1 - Resgatando Dados
-  useEffect(async() => {
+  const {data : items, httpConfig, loading} = useFetch(url);
 
-    const res = await fetch(url)
-    
-    const data = await res.json()
+{/*
+  useEffect(() => {
 
-    setProducts(data);
+    async function fetchData() {
 
+      const res = await fetch(url);
+      
+      const data = await res.json();
+
+      setProducts(data);
+  }
+
+  fetchData();
+ 
   }, []);
+ */}
 
-  console.log(products)
+  //2 - Adiconando produtos
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    }
+
+    httpConfig(product, "POST");
+
+  {/*
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+
+    
+
+    //3 - Carregamento Dinâmico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+*/}
+
+
+    setName("");
+    setPrice("");
+
+  }
+
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE");
+  };
+  
 
   return (
     <>
       <h1>Lista de Produtos</h1>
+      {/*6 - Loading*/}
+      {loading && <p>Carregando...</p>}
+      <ul>
+        {items && items.map((products)=> (<li key={products.id}>{products.name} - R$: {products.price}<button onClick={() => handleRemove(products.id)}>Excluir</button></li>))}
+        
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome: </span>
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label>
+            <span>Preço: </span>
+            <input type="text" value={price} name="price" onChange={(e) => setPrice(e.target.value)}  />
+          </label>
+
+          <input type="submit" value='criar' />
+
+        </form>
+      </div>
+
     </>
   )
 }
